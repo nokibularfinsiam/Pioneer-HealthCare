156a4338f1cf925788d43cfdeffb67f98f7f268f
Nokibul Arfin Siam <nokibularfinsiam@gmail.com>
2024-10-01 23:07:11 +0600

    Pioneer HealthCare

---
+++ WebApplication1/Controllers/AppointmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Context;
+
+namespace WebApplication1.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        ProjectDBContext _dbContext;
+        public AppointmentController()
+        {
+            this._dbContext = new ProjectDBContext();
+        }
+        public JsonResult GetAvailableTimes(int chamberId)
+        {
+            var availableTimes = _dbContext.TimeSlot.Where(t => t.ChamberId == chamberId).Select(t => new { Time = t.Time}).ToList();
+
+            return Json(new { success = true, data = availableTimes }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
